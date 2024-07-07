dd103ba8b5c4b96733506ba0919a04acbc204466
wangaodong <w1011782688@163.com>
2024-07-07 09:00:44 +0800

    渐变三角形封装后

--- src/glsl/util/index.js
+++ src/glsl/util/index.js
@@ -1,66 +1,63 @@
-// // 创建对象 传入源码 编译
-// /**
-//  * 
-//  * @param {WebGLRenderingContext} gl 
-//  * @param {*} type 
-//  * @param {*} source 
-//  */
-// export const createShader = function (gl, type, source) {
-//     // 根据类型创建shader
-//     let shader = gl.createShader(type);
-//     // shader其实就是字符串,传入shader
-//     gl.shaderSource(shader, source);
-//     // 在使用之前编译shader
-//     gl.compileShader(shader);
-//     return shader;
-// }
-
-// /**
-//  * 
-//  * @param {WebGLRenderingContext} gl 
-//  * @param {*} vertexShader 
-//  * @param {*} fragmentShader 
-//  */
-// export const createProgram = function (gl, vertexShader, fragmentShader) {
-//     let program = gl.createProgram();
-//     gl.attachShader(program, vertexShader);
-//     gl.attachShader(program, fragmentShader);
-//     gl.linkProgram(program);
-//     return program;
-// }
-
-// export const randomColor = function () {
-//     return {
-//         r: Math.random() * 255,
-//         g: Math.random() * 255,
-//         b: Math.random() * 255,
-//         a: 1
-//     }
-// }
 // 创建对象 传入源码 编译
-
+/**
+ * 
+ * @param {WebGLRenderingContext} gl 
+ * @param {*} type 
+ * @param {*} source 
+ */
 export const createShader = function (gl, type, source) {
-    var shader = gl.createShader(type);
+    // 根据类型创建shader
+    let shader = gl.createShader(type);
+    // shader其实就是字符串,传入shader
     gl.shaderSource(shader, source);
+    // 在使用之前编译shader
     gl.compileShader(shader);
     return shader;
 }
 
-
+/**
+ * 
+ * @param {WebGLRenderingContext} gl 
+ * @param {*} vertexShader 
+ * @param {*} fragmentShader 
+ */
 export const createProgram = function (gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
+    let program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
-    // debugger
     gl.linkProgram(program);
     return program;
 }
 
-export const randomColor = function() {
+export const randomColor = function () {
     return {
         r: Math.random() * 255,
         g: Math.random() * 255,
         b: Math.random() * 255,
         a: 1
     }
+}
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl 
+ * @param {*} attribute 
+ * @param {*} vertexAttribPointer 
+ */
+export function createBuffer(gl, attribute, vertexAttribPointer) {
+    let { size, type, normalize, stride, offset } = vertexAttribPointer;
+    gl.enableVertexAttribArray(attribute);
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+    // 多个buffer
+    gl.vertexAttribPointer(
+        attribute,
+        size,
+        type || gl.FLOAT,
+        normalize || false,
+        stride || 0,
+        offset || 0
+    );
+    return buffer;
 }
--- src/index.js
+++ src/index.js
@@ -1,224 +1,65 @@
-// import vert from './glsl//index.vert'
-// import frag from './glsl/index.frag'
-// import { createProgram, createShader, randomColor } from './glsl/util'
-
-// /**
-//  * @type {HTMLCanvasElement}
-//  */
-// const canvas = document.querySelector('#canvas')
-// let gl = canvas.getContext('webgl')
-
-// // 总的来讲一共分为四步
-// // 1. 先创建shader
-// const vertShader = createShader(gl, gl.VERTEX_SHADER, vert)
-// const fragShader = createShader(gl, gl.FRAGMENT_SHADER, frag)
-
-// // 2. 创建program来将shader附着到程序中
-// const program = createProgram(gl, vertShader, fragShader);
-// gl.useProgram(program)
-
-
-// const point = [];
-// const color = []
-
-// const a_position = gl.getAttribLocation(program, 'a_position');
-// const a_screen_size = gl.getAttribLocation(program, 'a_screen_size');
-// const a_color = gl.getAttribLocation(program, 'a_color')
-
-// // 给宽高赋值
-// gl.vertexAttrib2f(a_screen_size, canvas.width, canvas.height);
-
-// /**
-//  * 
-//  * @param {WebGLRenderingContext} gl 
-//  * @param {*} attribute 
-//  * @param {*} vertexAttribPointer 
-//  */
-// // function createBuffer(gl, attribute, vertexAttribPointer) {
-// //     let { size, type, normalize, stride, offset } = vertexAttribPointer;
-// //     gl.enableVertexAttribArray(attribute);
-// //     let buffer = gl.createBuffer();
-// //     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-// //     // 多个buffer
-// //     gl.vertexAttribPointer(
-// //         attribute,
-// //         size,
-// //         type || gl.FLOAT,
-// //         normalize || false,
-// //         stride || 0,
-// //         offset || 0
-// //     );
-// //     return buffer;
-// // }
-// function createBuffer(gl, attribute, vertexAttribPointer) {
-//     let { size, type, normalize, stride, offset } = vertexAttribPointer;
-//     gl.enableVertexAttribArray(attribute);
-//     let buffer = gl.createBuffer();
-//     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-//     // 多个buffer
-//     gl.vertexAttribPointer(
-//         attribute,
-//         size,
-//         type || gl.FLOAT,
-//         normalize || false,
-//         stride || 0,
-//         offset || 0
-//     );
-//     return buffer;
-// }
-
-
-
-// // // 定义颜色和位置的buffer并进行绑定
-// // const positionBuffer = gl.createBuffer();
-// // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-// // // 定义取值方式
-// // gl.vertexAttrib2f(a_position, 2, gl.FLOAT, false, 0, 0)
-
-// // const colorBuffer = gl.createBuffer();
-// // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-// // gl.vertexAttrib2f(a_color, 4, gl.FLOAT, false, 0, 0);
-
-// const positionBuffer = createBuffer(gl, a_position, {
-//     size: 2,
-//     type: gl.FLOAT,
-//     normalize: false,
-//     stride: 0,
-//     offset: 0,
-// })
-
-// const colorBuffer = createBuffer(gl, a_color, {
-//     size: 4,
-//     type: gl.FLOAT,
-//     normalize: false,
-//     stride: 0,
-//     offset: 0,
-// })
-
-// canvas.addEventListener('click', (e) => {
-//     point.push(e.pageX, e.pageY);
-//     const temp_color = randomColor();
-//     color.push(temp_color.r, temp_color.g, temp_color.b, temp_color.a);
-//     if (point.length > 0) {
-//         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-//         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW);
-
-//         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-//         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
-//         if (point.length % 6 == 0) {
-//             gl.clear(gl.COLOR_BUFFER_BIT);
-//             gl.drawArrays(gl.TRIANGLES, 0, point.length / 2);
-//         }
-//     }
-// })
-
-// gl.clearColor(0.0, 0.0, 0.0, 1.0)
-// gl.clear(gl.COLOR_BUFFER_BIT)
-
+import vert from './glsl//index.vert'
 import frag from './glsl/index.frag'
-import vert from './glsl/index.vert'
-import { createShader, createProgram, randomColor } from './glsl/util'
+import { createProgram, createShader, randomColor,createBuffer } from './glsl/util'
 
-var canvas = document.querySelector('#canvas');
-var gl = canvas.getContext('webgl') || canvas.getContext("experimental-webgl");
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.querySelector('#canvas')
+let gl = canvas.getContext('webgl')
 
+// 总的来讲一共分为四步
+// 1. 先创建shader
+const vertShader = createShader(gl, gl.VERTEX_SHADER, vert)
+const fragShader = createShader(gl, gl.FRAGMENT_SHADER, frag)
 
-// 创建顶点着色器
-const vertShader = createShader(gl, gl.VERTEX_SHADER, vert);
-const fragShader = createShader(gl, gl.FRAGMENT_SHADER, frag);
-
-// debugger
-//创建着色器程序
+// 2. 创建program来将shader附着到程序中
 const program = createProgram(gl, vertShader, fragShader);
-// 使用刚创建好的着色器程序。
-gl.useProgram(program);
+gl.useProgram(program)
 
-// let success = gl.getProgramParameter(program, gl.LINK_STATUS);
-// debugger
-// debugger
 
-// 定义三个顶点  传入的canvas的x和y
-const point = []
+const point = [];
 const color = []
 
-// 读取posiotion地址
 const a_position = gl.getAttribLocation(program, 'a_position');
-const a_color = gl.getAttribLocation(program, 'a_color');
-// 读取宽高
+const a_color = gl.getAttribLocation(program, 'a_color')
 const a_screen_size = gl.getAttribLocation(program, 'a_screen_size');
 
-// debugger
 // 给宽高赋值
-// 传静态顶点信息
 gl.vertexAttrib2f(a_screen_size, canvas.width, canvas.height);
-
-
-// 定义颜色和位置的buffer数据绑定
-function createBuffer(gl, attribute, vertexAttribPointer) {
-    let { size, type, normalize, stride, offset } = vertexAttribPointer;
-    gl.enableVertexAttribArray(attribute);
-    let buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-    // 多个buffer
-    gl.vertexAttribPointer(
-        attribute,
-        size,
-        type || gl.FLOAT,
-        normalize || false,
-        stride || 0,
-        offset || 0
-    );
-    return buffer;
-}
-
 
 const positionBuffer = createBuffer(gl, a_position, {
     size: 2,
     type: gl.FLOAT,
     normalize: false,
     stride: 0,
-    offset: 0
-});
+    offset: 0,
+})
 
 const colorBuffer = createBuffer(gl, a_color, {
     size: 4,
     type: gl.FLOAT,
     normalize: false,
     stride: 0,
-    offset: 0
-});
+    offset: 0,
+})
 
-
-canvas.addEventListener('click', function (e) {
-    const x = e.pageX;
-    const y = e.pageY;
-    point.push(x, y);
-
+canvas.addEventListener('click', (e) => {
+    point.push(e.pageX, e.pageY);
     const temp_color = randomColor();
     color.push(temp_color.r, temp_color.g, temp_color.b, temp_color.a);
-
     if (point.length > 0) {
-        // 每次要重新bindBuffer
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        // 将动态改为静态
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
-
-        if (point.length % 6 === 0) {
+        if (point.length % 6 == 0) {
             gl.clear(gl.COLOR_BUFFER_BIT);
             gl.drawArrays(gl.TRIANGLES, 0, point.length / 2);
         }
     }
 })
 
-
-// 清空画布
-gl.clearColor(0, 0, 0, 1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-
+gl.clearColor(0.0, 0.0, 0.0, 1.0)
+gl.clear(gl.COLOR_BUFFER_BIT)
